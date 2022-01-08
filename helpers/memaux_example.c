512135d7933bf41382c5a545fb5a169754f4eafc
darkArp <mario@whitehathacking.tech>
2022-01-08 14:00:45 +0000

    Update memaux_example.c

--- helpers/memaux_example.c
+++ helpers/memaux_example.c
@@ -10,28 +10,35 @@ int main()
         .real_size = 0,
         .total_size = 0};
 
-    for (int i = 0; i < 5; i++)
+    printf("Appending to the list:\n");
+
+    for (int i = 0; i < 10; i++)
     {
         append(&list, i); // Adding new valued to List
         printf("Real size: %d | Total size: %d\n", list.real_size, list.total_size);
     }
 
     // Printing the List
+
+    printf("List after appending:\n");
     for (int i = 0; i < list.real_size; i++)
     {
         printf("%d\n", list.ptr[i]);
     }
 
-    printf("Old size: %d\n", list.real_size);
-
+    printf("\nPopping elements at index %d and %d:\n", 0, 1);
+    printf("Old real size: %d\n", list.real_size);
     printf("Popped element: %d\n", pop(&list, 0)); // Removing element from List at index
     printf("Popped element: %d\n", pop(&list, 1)); // Removing element from List at index
 
-    printf("New size: %d\n", list.real_size);
+    printf("New real size: %d\n", list.real_size);
 
-    for (int i = 0; i < list.real_size; i++)
+    printf("\nPopping memory management:\n");
+    for (int i = 2; i < 10; i++)
     {
-        printf("%d\n", list.ptr[i]);
+        pop(&list, i); // Adding new valued to List
+        printf("Real size: %d | Total size: %d\n", list.real_size, list.total_size);
     }
+
     free(list.ptr);
 }
