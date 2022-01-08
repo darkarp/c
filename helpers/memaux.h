67d87eb6e226168ad079b50d26e1c8a2ea4ec4ed
darkArp <mario@whitehathacking.tech>
2022-01-08 13:44:09 +0000

    Update memaux.h

--- helpers/memaux.h
+++ helpers/memaux.h
@@ -12,13 +12,13 @@ int append(List *list, int value)
     size_t t_size = list->total_size, r_size = list->real_size;
     size_t new_r_size = r_size + 1, new_t_size = (new_r_size + t_size) * 2;
 
-    if (t_size <= new_r_size)
+    if (t_size <= new_r_size) // checking to see if we already have big enough size to add new element
     {
-        void *new_ptr = realloc(list->ptr, new_t_size * sizeof(int));
+        void *new_ptr = realloc(list->ptr, new_t_size * sizeof(int)); // leveraging the fact that realloc with a NULL ptr is just a malloc
         if (new_ptr)
         {
-            list->ptr = (int *)new_ptr;
-            list->total_size = new_t_size;
+            list->ptr = (int *)new_ptr;    // update ptr after realloc
+            list->total_size = new_t_size; // updating total size
         }
         else
         {
@@ -26,8 +26,8 @@ int append(List *list, int value)
         }
     }
 
-    list->ptr[r_size] = value;
-    list->real_size = new_r_size;
+    list->ptr[r_size] = value;    // adding the element to the List
+    list->real_size = new_r_size; // realsize increments by 1
     return 0;
 }
 
@@ -38,9 +38,11 @@ int pop(List *list, int index)
     if (r_size > 0)
     {
         list->real_size -= 1;
+
+        // Shifting the values so we don't have holes
         for (int i = index; i < r_size - 1; i++)
             list->ptr[i] = list->ptr[i + 1];
         return elem;
     }
-    return -1;
+    return -1; // Can't pop if it's empty
 }
